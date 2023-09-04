2e03588e4d30fb56f7a47399d57191b6b1d897b3
Jennyma <jennymainfini@gmail.com>
2023-09-04 03:34:06 -0700

    update seeder file

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,16 +11,6 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
       username: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -43,6 +33,17 @@ module.exports = (sequelize, DataTypes) => {
           isEmail: true
         }
       },
+
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
       hashedPassword: {
         type: DataTypes.STRING.BINARY,
         allowNull: false,
